685f56e09e9355cb0863fc325952e0f16dd1515e
Dominic Freeman <dominic.freeman123@gmail.com>
2021-03-26 14:58:50 +0000

    Load plans in map component and render a test marker (wip)

--- components/Map.jsx
+++ components/Map.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import "leaflet/dist/leaflet.css"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 
-const Map = () => (
+const Map = ({plans}) => (
   <MapContainer
     style={{ height: "300px" }}
     center={[51.505, -0.09]}
@@ -13,7 +13,7 @@ const Map = () => (
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={[51.505, -0.09]}>
+    <Marker position={[51.511549161417705, -0.12814928764125325]}>
       <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
       </Popup>
